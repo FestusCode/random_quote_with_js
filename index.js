9474d6cb81f99a92c8e136edc43fa065ebdb65c0
FestusCode <festusdjossou@gmail.com>
2022-09-28 00:55:38 +0200

    click listner added to btn with getData function

--- index.js
+++ index.js
@@ -1,9 +1,15 @@
 let quote = document.querySelector("#quote");
 let author = document.querySelector("#author");
 let btn = document.querySelector("#btn");
-fetch("https://api.quotable.io/random")
-  .then((response) => response.json())
-  .then((data) => {
-    quote.innerHTML = data.content;
-    author.innerHTML = data.author;
-  });
+function getData() {
+  fetch("https://api.quotable.io/random")
+    .then((response) => response.json())
+    .then((data) => {
+      quote.innerHTML = data.content;
+      author.innerHTML = data.author;
+    });
+}
+getData();
+addEventListener("click", () => {
+  getData();
+});
